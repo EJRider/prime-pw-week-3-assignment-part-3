cf48463a1bbf2be0e17a88a88d3355582d742ee3
Rider <goejrider@gmail.com>
2022-08-30 17:48:09 -0500

    Base work finished

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -9,24 +9,36 @@ console.log (partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = ['3', '5', '-6', '0', '7', '11'];
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
 console.log (supplyChanges[1]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
-
+let removedItem = supplyChanges.pop()
+console.log ('This has been removed', removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-
+supplyChanges.push (25);
+console.log (supplyChanges);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (x=0; x<supplyChanges.length; x++ ) {
+    if (supplyChanges[x]>0) {
+        console.log ('Added', supplyChanges[x],'parts');
+    }
+    if (supplyChanges[x]=== 0) {
+        console.log ('No change');
+    }
+    if (supplyChanges[x]<0){
+        console.log ('Removed', supplyChanges[x]*-1,'parts');
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
